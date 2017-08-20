44976642f99a36f23e8cf5208f31ee9d3c7acf08
Matthew <cygnusvis@gmail.com>
2017-08-20 02:03:43 -0700

    added licensing info.

--- Cygnet3/Tag.hpp
+++ Cygnet3/Tag.hpp
@@ -1,3 +1,23 @@
+/*
+
+(C) Matthew Swanson
+
+This file is part of Cygnet3.
+
+Cygnet3 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+Cygnet3 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Cygnet3.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #pragma once
 
 #include <cstddef>
